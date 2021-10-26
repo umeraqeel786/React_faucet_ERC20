2cfd9b491f24827681f957743ad097ec9f91a221
Umer Aqeel <31266008+umeraqeel786@users.noreply.github.com>
2021-10-26 12:56:50 +0500

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeyby: {
+      url: "https://rinkeby.infura.io/v3/54864fd661734a288da4dcdda1ad2f7c",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.3"
+};
